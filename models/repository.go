41a0e52653fb4e53fb6a3d6e7edde68c1bed6d42
glebateee <glebstanyul2003@gmail.com>
2025-10-06 22:49:57 +0300

    added admin

--- models/repository.go
+++ models/repository.go
@@ -3,7 +3,9 @@ package models
 type Repository interface {
 	GetProduct(id int) Product
 	GetProducts() []Product
+	SaveProduct(*Product)
 	GetCategories() []Category
+	SaveCategory(*Category)
 	Seed()
 
 	GetProductPage(page, pageSize int) (products []Product, totalAvailable int)
